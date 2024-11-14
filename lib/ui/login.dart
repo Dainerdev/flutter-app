f4c35be0fed73b833f2741a334f4aa3fc46038e9
Dainerdev <florianarroyo15@gmail.com>
2024-11-13 21:24:48 -0500

    Se aplica diseño para el formulario

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../main.dart';
 
 class LoginView extends StatelessWidget {
   
@@ -6,29 +7,95 @@ class LoginView extends StatelessWidget {
   static String id = 'login_view';
 
   @override
-  Widget build(BuildContext context) {
-    
+  Widget build(BuildContext context) {    
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // email
-            TextField(
-              onChanged: (value) {}
-            ),
-
-            // password
-            TextField(
-              onChanged: (value) {},
-            ),
-
-            // button
-            ElevatedButton(
-              onPressed: ()=>{}, 
-              child: const Text('Iniciar Sesión')
-            )
-          ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Inicia Sesión',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 141, 74, 180),
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 50),
+              //email
+              TextField(
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.email_outlined),
+                  labelText: 'Email',
+                  hintText: 'ejemplo.correo@gmail.com'
+                ),
+                onChanged: (value) {}
+              ),
+              
+              const SizedBox(height: 20),
+              // password
+              TextField(
+                decoration: const InputDecoration(                  
+                  prefixIcon: Icon(Icons.password_outlined),
+                  labelText: 'Contraseña',                  
+                ),
+                onChanged: (value) {},
+                obscureText: true,
+              ),
+              const SizedBox(height: 30),
+              // button
+              ElevatedButton(
+                onPressed: (){
+        
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 141, 74, 180),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 100, vertical: 15
+                  ),
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                ),
+                child: const Text('Iniciar Sesión',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const MainApp())                  
+                  );
+                }, 
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 245, 230, 253),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 100, vertical: 15
+                  ),
+                  minimumSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                ),
+                child: const Text('Regresar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 141, 74, 180),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
