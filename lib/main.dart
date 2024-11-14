a2921c61601574a80d089c37f460537c9f876893
Dainerdev <florianarroyo15@gmail.com>
2024-11-13 20:16:23 -0500

    Se desarrolla la clase HomeScreen que contiene el widget de la vista home

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,79 @@
 import 'package:flutter/material.dart';
+import 'ui/login.dart';
+import 'ui/register.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello world!'),
+     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Main',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Column(
+              children: [
+                Text(
+                  'Bienvenido!',
+                  style: TextStyle(
+                    color: Colors.brown,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '¿Qué deseas realizar?',
+                  style: TextStyle(
+                    fontSize: 25
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const LoginView()),
+                    );
+                  },
+                  child: const Text('Iniciar Sesión'),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) =>  RegisterView()),
+                    );
+                  },
+                  child: const Text('Regístrate'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
