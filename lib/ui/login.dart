1ff34c3f9ebd122c767e51aa552589aa1ac6db51
Dainerdev <florianarroyo15@gmail.com>
2024-11-14 23:44:58 -0500

    Se hace uso del paquete http para comunicar la app con la api. Se desarrolla funcion login() para enviar valores de validacion a la api. Se desarrolla funcion _useLogin() para aplicar validaciones del login a la app

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -1,28 +1,143 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_und3/main.dart';
 import 'home.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class LoginScreen extends StatefulWidget {
-  
   const LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
-  
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+  // funcion para loguear
+  Future<Map<String, dynamic>> login(String ema, String pass) async {
 
+    final apiUrl = Uri.parse('http://10.0.2.2:3312/api/users/login');
+
+    final response = await http.post(apiUrl,
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"email": ema, "password": pass}),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Error al iniciar sesión.');
+    }
+  }
+
+class _LoginScreenState extends State<LoginScreen> {
   // Identificadores de los textfields
   final email = TextEditingController();
   final password = TextEditingController();
 
-  //variables
-  String ema = '';
-  String pass = '';
+  Future<void> _useLogin() async {
+    final ema = email.text;
+    final pass = password.text;
+
+    try {
+      final result = await login(ema, pass);
+
+      if (result.containsKey('id')) {
+        // Usuario existe
+        showDialog(
+          // ignore: use_build_context_synchronously
+          context: context,
+            barrierDismissible: false,
+            barrierColor: Color.fromARGB(180, 0, 0, 0),
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(
+                  'Notificación!',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  content: SingleChildScrollView(
+                    child: ListBody(
+                      children: [
+                        Text(
+                          'Inicio de sesión exitoso.',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text('Bienvenido ${result['email']}',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                            const HomeScreen()));
+                  },
+                  child: Text(
+                    'Continuar',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            );
+          }
+        );
+      } else {
+        // Usuario no existe
+        showDialog(
+          // ignore: use_build_context_synchronously
+          context: context,
+          barrierDismissible: false,
+          barrierColor: Color.fromARGB(180, 0, 0, 0),
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text(
+                'Notificación!',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: [
+                    Text(
+                      'Credeniales incorrectas. Verifique e inténtelo nuevamente.',
+                      style: TextStyle(fontSize: 16),
+                    )
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Entiendo',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  )),
+                ],
+              );
+            });
+      }
+
+    } catch (error) {
+      // error
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $error')),
+      );
+    }
+  }
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
@@ -43,20 +158,18 @@ class _LoginScreenState extends State<LoginScreen> {
               TextField(
                 controller: email,
                 decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.email_outlined),
-                  labelText: 'Correo',
-                  hintText: 'ejemplo.correo@gmail.com'
-                ),
+                    prefixIcon: Icon(Icons.email_outlined),
+                    labelText: 'Correo',
+                    hintText: 'ejemplo.correo@gmail.com'),
                 onChanged: (value) {},
-
-              ),              
+              ),
               const SizedBox(height: 20),
               // password
               TextField(
                 controller: password,
-                decoration: const InputDecoration(                  
+                decoration: const InputDecoration(
                   prefixIcon: Icon(Icons.password_outlined),
-                  labelText: 'Contraseña',                  
+                  labelText: 'Contraseña',
                 ),
                 onChanged: (value) {},
                 obscureText: true,
@@ -65,135 +178,53 @@ class _LoginScreenState extends State<LoginScreen> {
               // button
               ElevatedButton(
                 onPressed: () {
-
                   //Guardar los datos en las variables
-                  ema = email.text;
-                  pass = password.text;
+                  var ema = email.text;
+                  var pass = password.text;
 
                   // Validar que los campos no esten vacios
                   if (ema == '' || pass == '') {
                     showDialog(
-                      context: context,
-                      barrierDismissible: false,                      
-                      barrierColor: Color.fromARGB(180, 0, 0, 0),
-                      builder: (BuildContext context){
-                        return AlertDialog(
-                          title: Text('Notificación!',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold
+                        context: context,
+                        barrierDismissible: false,
+                        barrierColor: Color.fromARGB(180, 0, 0, 0),
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text(
+                              'Notificación!',
+                              style: TextStyle(fontWeight: FontWeight.bold),
                             ),
-                          ),
-                          content: SingleChildScrollView(
-                            child: ListBody(
-                              children: [
-                                Text('Por favor, ingrese los datos de usuario.',
-                                  style: TextStyle(
-                                    fontSize: 16
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              }, 
-                              child: Text('Entiendo',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold
-                                ),
-                              )
-                            ),
-                          ],
-                        );
-                      }
-                    );
-                  } else if (ema == 'yo' && pass == '123') { //Validar que los datos pertenezcan a un usuario
-                    showDialog(
-                      context: context, 
-                      barrierDismissible: false,
-                      barrierColor: Color.fromARGB(180, 0, 0, 0),
-                      builder: (BuildContext context){
-                        return AlertDialog(
-                          title: Text('Notificación!',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          content: SingleChildScrollView(
-                            child: ListBody(
-                              children: [
-                                Text('Inicio de sesión correcto.',
-                                  style: TextStyle(
-                                    fontSize: 16
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                                Navigator.push(
-                                  context, 
-                                  MaterialPageRoute(
-                                    builder: (context) => const HomeScreen())
-                                );                                
-                              }, 
-                              child: Text('Continuar',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold
-                                ),
+                            content: SingleChildScrollView(
+                              child: ListBody(
+                                children: [
+                                  Text(
+                                    'Por favor, ingrese los datos de usuario.',
+                                    style: TextStyle(fontSize: 16),
+                                  )
+                                ],
                               ),
                             ),
-                          ],
-                        );
-                      }
-                    );
-                  } else {
-                    showDialog(
-                      context: context, 
-                      barrierDismissible: false,
-                      barrierColor: Color.fromARGB(180, 0, 0, 0),
-                      builder: (BuildContext context){
-                        return AlertDialog(
-                          title: Text('Notificación!',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          content: SingleChildScrollView(
-                            child: ListBody(
-                              children: [
-                                Text('Credeniales incorrectas. Verifique e inténtelo nuevamente.',
-                                  style: TextStyle(
-                                    fontSize: 16
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              }, 
-                              child: Text('Entiendo',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold
-                                ),
-                              )
-                            ),
-                          ],
-                        );
-                      }
-                    );
-                  } 
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text(
+                                    'Entiendo',
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  )),
+                            ],
+                          );
+                    });
+                  
+                  } else if (ema != '' && pass != '') {
+                    //Validar que los datos pertenezcan a un usuario                    
+                    _useLogin();
+                    
+                    login(ema, pass);                     
+                  }
 
                   // Vaciar los campos
                   email.text = '';
@@ -201,15 +232,14 @@ class _LoginScreenState extends State<LoginScreen> {
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromARGB(255, 141, 74, 180),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 100, vertical: 15
-                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
                   minimumSize: const Size(double.infinity, 50),
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)
-                  ),
+                      borderRadius: BorderRadius.circular(15)),
                 ),
-                child: const Text('Iniciar Sesión',
+                child: const Text(
+                  'Iniciar Sesión',
                   style: TextStyle(
                     fontSize: 20,
                     color: Colors.white,
@@ -219,23 +249,20 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: (){
-                  Navigator.push(
-                    context, 
-                    MaterialPageRoute(builder: (context) => const MainApp())
-                  );
-                }, 
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const MainApp()));
+                },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: const Color.fromARGB(255, 245, 230, 253),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 100, vertical: 15
-                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 100, vertical: 15),
                   minimumSize: const Size(double.infinity, 50),
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15)
-                  ),
+                      borderRadius: BorderRadius.circular(15)),
                 ),
-                child: const Text('Regresar',
+                child: const Text(
+                  'Regresar',
                   style: TextStyle(
                     fontSize: 20,
                     color: Color.fromARGB(255, 141, 74, 180),
@@ -250,10 +277,3 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 }
-
-
-
-
-
-  
-  
